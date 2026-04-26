ce6840309e9b58517c54a6e223a898c6b4791c14
UniqueTechnology395 <UniqueTech2025@outlook.com.gr>
2026-04-26 11:45:59 +0300

    Update Blink.ino

--- src/Blink.ino
+++ src/Blink.ino
@@ -1,4 +1,19 @@
-// Μάθημα 1.3 - Unique Tech Blink
+/* * ----------------------------------------------------------------------------
+ * Project: Unique Tech Blink (Basic)
+ * Author: Unique Tech Team
+ * License: MIT License
+ * * Copyright (c) 2026 Unique Tech
+ * * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * ----------------------------------------------------------------------------
+ */
+
 void setup() {
   pinMode(13, OUTPUT); // Προετοιμασία του Pin 13 ως Έξοδο
 }
